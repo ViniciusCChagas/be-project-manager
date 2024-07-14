d1c5e02c06c10bae148d11010283194765884b55
Vinicius Chagas <vinicius_decastro@hotmail.com>
2024-07-14 16:47:49 -0300

    cookie test

--- src/useCases/Authentication/Login/LoginController.ts
+++ src/useCases/Authentication/Login/LoginController.ts
@@ -27,6 +27,7 @@ export class LoginController {
             httpOnly: true,
             maxAge: 1000 * 60 * 60 * 8,
             path: '/',
+            sameSite: 'none',
             secure: process.env.NODE_ENV === 'production',
         })
 
